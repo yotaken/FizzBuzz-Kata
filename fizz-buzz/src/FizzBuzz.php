2a10f6c57e28c48c978bc6114d0110dce3b01c28
yotaken <yotaone@gmail.com>
2015-12-20 19:55:45 +0100

    Object calisthenics - creation method and remove responsability from getData.
     - hasThreeInIt

--- fizz-buzz/src/FizzBuzz.php
+++ fizz-buzz/src/FizzBuzz.php
@@ -50,19 +50,27 @@ class FizzBuzz
     }
 
     /**
+     * @return mixed
+     */
+    private function hasThreeInIt($pos)
+    {
+        $hasThreeInIt = strpos((string)$pos, '3') !== false;
+        return $hasThreeInIt;
+    }
+
+    /**
      * @return array
      */
     public function getData()
     {
         $numbers = array();
         for($i = 1; $i < 101; $i++){
-            $divider = true;
             $numbers[$i] = $i;
             $numbers = $this->divisibleByThreeSetsPositionAsFizz($i,$numbers);
             $numbers = $this->divisibleByFiveSetsPositionAsBuzz($i,$numbers);
             $numbers = $this->divisibleByFiveAndThreeSetsPositionAsFizzBuzz($i,$numbers);
             $divider = $this->isDivisibleByThreeOrFive($i);
-            $has3InIt = strpos((string)$i, '3') !== false;
+            $has3InIt = $this->hasThreeInIt($i);
             $has5InIt = strpos((string)$i, '5') !== false;
             if (!$divider && $has3InIt && !$has5InIt) { $numbers[$i] = 'Fizz'; }
             if (!$divider && !$has3InIt && $has5InIt) { $numbers[$i] = 'Buzz'; }
